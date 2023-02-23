22c567dc91aded485a0beebd55ded8d94453cc76
Konstantin Kobliuck <kkobliuk@gmail.com>
2023-02-23 15:46:26 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        let min = this.min;
+        let max = this.max;
 
+        this.result = Math.round((min + max) / 2);
+        return this.result; 
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
